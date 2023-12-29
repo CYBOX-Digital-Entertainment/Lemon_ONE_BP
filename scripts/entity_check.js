09daeb8eec3d5dabd91d1f537bdcbd14a069deb7
lemon7679 <145438843+lemon7679@users.noreply.github.com>
2023-12-29 21:53:34 +0900

    update

--- scripts/entity_check.js
+++
@@ -1,9 +0,0 @@
-import { world } from "@minecraft/server";
-export function entity_check(pl) {
-    pl.forEach(pl => {
-        pl.getTags().filter(f => f.startsWith(`id:`)).forEach(f => {
-            if (world.getEntity(f.slice(3)) == undefined)
-                pl.removeTag(f);
-        });
-    });
-}
--- scripts/entity_check.ts
+++
@@ -1,9 +0,0 @@
- import { Player, world } from "@minecraft/server"
- 
- export function entity_check(pl: Player[]){
-    pl.forEach(pl=>{
-        pl.getTags().filter(f=>f.startsWith(`id:`)).forEach(f=>{
-            if(world.getEntity(f.slice(3)) == undefined) pl.removeTag(f)
-        })
-    })
- }
